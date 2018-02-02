db6d78a0ce83fab804766a6c6df0833da7b59d7a
Andy Maskiell <maskiella@gmail.com>
2018-02-02 22:53:46 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   belongs_to :network
 
-  def build_network(hash)
+  def build_network(netw_hash)
     self.network.call_letters = hash.values.first
   end
 end
