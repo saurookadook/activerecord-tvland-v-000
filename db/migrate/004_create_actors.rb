805a18973d7f1980aad55f20445f0ce3f93d0f46
Andy Maskiell <maskiella@gmail.com>
2018-02-02 22:04:59 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -3,6 +3,8 @@ class CreateActors < ActiveRecord::Migration[4.2]
     create_table :actors do |t|
       t.string :first_name
       t.string :last_name
+      t.integer :character_id
+      t.integer :show_id
     end
   end
 end
