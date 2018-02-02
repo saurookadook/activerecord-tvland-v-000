a3af4a59d1b354dcb6c9ad11c408a73d0c836bb2
Andy Maskiell <maskiella@gmail.com>
2018-02-02 22:58:12 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -7,7 +7,7 @@ class Character < ActiveRecord::Base
   end
 
   def build_show(show_hash)
-    new_show = Show.find_by(show_hash)
+    show = Show.find_by(show_hash)
     self.show_id = new_show.id
   end
 end
